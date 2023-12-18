1086886104a076c90f3c46dd3d4edea41aa02146
bukhoree032 <63540507+bukhoree032@users.noreply.github.com>
2023-12-18 11:22:09 +0700

    อัพเดท excell

--- Modules/Manage/Http/Controllers/Secure_scorController.php
+++ Modules/Manage/Http/Controllers/Secure_scorController.php
@@ -94,7 +94,7 @@ class Secure_scorController extends UploadeFileController
                     // บันทึกการประชุม
 
                     $data['result'] = \DB::table('meeting')
-                                    ->join('activitymeeting', 'activitymeeting.id_meet', '=', 'meeting.id_user')
+                                    ->LEFTJOIN('activitymeeting', 'meeting.id_user', '=', 'activitymeeting.id_meet')
                                     ->where('meeting.meeting_date', 'like', $timeth.'%')
                                     ->where('meeting.id_user' ,$value->id)
                                     ->get();
@@ -107,13 +107,15 @@ class Secure_scorController extends UploadeFileController
                     // บันทึกการประชุม
 
                     // รูปภาพ
-                    $data['user'][$key]->$mi['picture_meet'] = 'ไม่มี';
 
                     foreach ($data['result'] as $key_result => $value_result) {
 
                         if($value_result->picture_meet != '' AND $value_result->picture_meet != 's:0:"";'){
                             $data['user'][$key]->$mi['picture_meet'] = 'มี';
                         }
+                    }
+                    if(!isset($data['user'][$key]->$mi['picture_meet'])){
+                        $data['user'][$key]->$mi['picture_meet'] = 'ไม่มี';
                     }
                     // รูปภาพ
                 
@@ -192,7 +194,7 @@ class Secure_scorController extends UploadeFileController
                 // บันทึกการประชุม
 
                 $data['result'] = \DB::table('meeting')
-                                ->join('activitymeeting', 'activitymeeting.id_meet', '=', 'meeting.id_user')
+                                ->LEFTJOIN('activitymeeting', 'meeting.id_user', '=', 'activitymeeting.id_meet')
                                 ->where('meeting.meeting_date', 'like', $timeth.'%')
                                 ->where('meeting.id_user' ,$value->id)
                                 ->get();
@@ -205,7 +207,6 @@ class Secure_scorController extends UploadeFileController
                 // บันทึกการประชุม
 
                 // รูปภาพ
-                $data['user'][$key]->$mi['picture_meet'] = 'ไม่มี';
 
                 foreach ($data['result'] as $key_result => $value_result) {
 
@@ -213,98 +214,12 @@ class Secure_scorController extends UploadeFileController
                         $data['user'][$key]->$mi['picture_meet'] = 'มี';
                     }
                 }
+                
+                if(!isset($data['user'][$key]->$mi['picture_meet'])){
+                    $data['user'][$key]->$mi['picture_meet'] = 'ไม่มี';
+                }
                 // รูปภาพ
             
-        }
-        return view('admin::secure_scor.secure_scor',$data);
-    }
-
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    public function index_search1(Request $request)
-    {
-        // dd($request->cars);
-        $id =1 ;
-        if(Auth::user()->status != 5){
-            return redirect()->route('index.meeting');
-        }
-        
-        $data['pro'] = \DB::table('users')
-                            ->select('provinces')
-                            ->groupBy('provinces')
-                        ->get();
-
-        $time = $request->cars;
-// dd($time);
-        $data['id'] = $id;
-        $data['time'] = $time;
-        
-        $timeth=date_create($data['time']);
-        $timeth = date_format($timeth,"Y")+'543'."-".date_format($timeth,"m");
-
-        $row = 0;
-
-        foreach ($data['pro'] as $key_pro => $value_pro) {
-            $data['meet'][$value_pro->provinces]['narcotics'] = 0;
-            $data['meet'][$value_pro->provinces]['unrest'] = 0;
-            $data['meet'][$value_pro->provinces]['guard'] = 0;
-            $data['meet'][$value_pro->provinces]['covid'] = 0;
-            // $data['meet'][$value_pro->provinces]['picture_meet'] = 0;
-        
-            $data['result'] = \DB::table('meeting')
-                                ->select('users.id as id_user','meeting.id as id_meet',
-                                'meeting.narcotics','meeting.unrest','meeting.guard','meeting.covid','activitymeeting.picture_meet',
-                                'users.provinces','users.amphures','users.districts','users.status')
-                                ->join('activitymeeting', 'activitymeeting.id_meet', '=', 'meeting.id_user')
-                                ->join('users', 'users.id', '=', 'meeting.id_user')
-                                ->where('meeting.meeting_date', 'like', $timeth.'%')
-                                ->where('users.provinces', $value_pro->provinces)
-                                ->where('users.status', '!=' ,'5')
-                                ->ORDERBY('amphures','deSC')
-                                // ->where('meeting.id_user' ,$value->id)
-                                ->get();
-
-            foreach ($data['result'] as $key => $value) {
-                $data['data_meet'][$row] = $value;
-                $row ++;
-                if($value->narcotics != ''){
-                    $value->narcotics = 'มี';
-                    $data['meet'][$value_pro->provinces]['narcotics'] ++;
-                }else{
-                    $value->narcotics = 'ไม่มี';
-                }
-                
-                if($value->unrest != ''){
-                    $value->unrest = 'มี';
-                    $data['meet'][$value_pro->provinces]['unrest'] ++;
-                }else{
-                    $value->unrest = 'ไม่มี';
-                }
-                
-                if($value->guard != ''){
-                    $value->guard = 'มี';
-                    $data['meet'][$value_pro->provinces]['guard'] ++;
-                }else{
-                    $value->guard = 'ไม่มี';
-                }
-                
-                if($value->covid != ''){
-                    $value->covid = 'มี';
-                    $data['meet'][$value_pro->provinces]['covid'] ++;
-                }else{
-                    $value->covid = 'ไม่มี';
-                }
-                
-                // if($value->picture_meet != '' AND $value->picture_meet != 's:0:"";'){
-                //     // dd($value->picture_meet);
-                //     $value->picture_meet = 'มี';
-                //     $data['meet'][$value_pro->provinces]['picture_meet'] ++;
-                // }else{
-                //     $value->picture_meet = 'ไม่มี';
-                // }
-            }
         }
         return view('admin::secure_scor.secure_scor',$data);
     }
@@ -378,7 +293,7 @@ class Secure_scorController extends UploadeFileController
                     // บันทึกการประชุม
 
                     $data['result'] = \DB::table('meeting')
-                                    ->join('activitymeeting', 'activitymeeting.id_meet', '=', 'meeting.id_user')
+                                    ->LEFTJOIN('activitymeeting', 'meeting.id_user', '=', 'activitymeeting.id_meet')
                                     ->where('meeting.meeting_date', 'like', $timeth.'%')
                                     ->where('meeting.id_user' ,$value->id)
                                     ->get();
@@ -399,121 +314,14 @@ class Secure_scorController extends UploadeFileController
                             $data['user'][$key]->$mi['picture_meet'] = '1';
                         }
                     }
+                    if(!isset($data['user'][$key]->$mi['picture_meet'])){
+                        $data['user'][$key]->$mi['picture_meet'] = '0';
+                    }
                     // รูปภาพ
                 
             }
             return view('admin::secure_scor.secure_scor_excell',$data);
     }
-    
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    // public function excell1($time)
-    // {
-    //     $id =1 ;
-    //         if(Auth::user()->status != 5){
-    //             return redirect()->route('index.meeting');
-    //         }
-            
-    //         $data['pro'] = \DB::table('users')
-    //                             ->select('provinces')
-    //                             ->groupBy('provinces')
-    //                         ->get();
-    
-    //         $data['user'] = \DB::table('users')
-    //                             ->select('id','provinces','amphures','districts','status')
-    //                             // ->where('status' ,$id)
-    //                             ->where('users.status', '!=' ,'5')
-    //                             ->ORDERBY('provinces','deSC')
-    //                             ->ORDERBY('amphures','deSC')
-    //                             ->get();
-            
-    //         $y = date('Y');
-    //         $m = date('m');
-    //         for ($i_y='2023'; $i_y <= $y; $i_y++) { 
-    //             for ($i_m = '06'; $i_m <= '7'; $i_m++) { 
-    //                 $time = $i_y.'-'.$i_m;
-
-
-    //                     foreach ($data['user'] as $key => $value) {
-
-    //                             $data['id'] = $id;
-    //                             $data['time'] = $time;
-                                
-    //                             $timeth=date_create($data['time']);
-
-    //                             // รายงานการประชุม
-    //                             $time_y = date_format($timeth,"Y")+'543';
-    //                             $time_m = date_format($timeth,"m");
-    //                             $mont = [
-    //                                 '01'=>'มกราคม',
-    //                                 '02'=>'กุมภาพันธ์',
-    //                                 '03'=>'มีนาคม',
-    //                                 '04'=>'เมษายน',
-    //                                 '05'=>'พฤษภาคม',
-    //                                 '06'=>'มิถุนายน',
-    //                                 '07'=>'กรกฎาคม',
-    //                                 '08'=>'สิงหาคม',
-    //                                 '09'=>'กันยายน',
-    //                                 '10'=>'ตุลาคม',
-    //                                 '11'=>'พฤศจิกายน',
-    //                                 '12'=>'ธันวาคม',
-    //                             ];
-    //                             $data['result'] = \DB::table('reportmeeting')
-    //                                 ->select('id')
-    //                                 ->where('reportmeeting.month', $mont[$time_m])
-    //                                 ->where('reportmeeting.year', $time_y)
-    //                                 ->where('reportmeeting.id_user' ,$value->id)
-    //                                 ->get();  
-                                
-    //                             $timeth = date_format($timeth,"Y")+'543'."-".date_format($timeth,"m");
-    //                             $data['user'][$key]->date = $timeth;
-
-    //                             $mi = 'secure';
-    //                             if(empty($data['result']['0'])){
-    //                                 $data['user'][$key]->$mi[$timeth]['report'] = '0';
-    //                             }else{
-    //                                 $data['user'][$key]->$mi[$timeth]['report'] = '1';
-    //                             }
-    //                             // รายงานการประชุม
-
-    //                             // บันทึกการประชุม
-
-    //                             $data['result'] = \DB::table('meeting')
-    //                                             ->join('activitymeeting', 'activitymeeting.id_meet', '=', 'meeting.id_user')
-    //                                             ->where('meeting.meeting_date', 'like', $timeth.'%')
-    //                                             ->where('meeting.id_user' ,$value->id)
-    //                                             ->get();
-    //                             if(empty($data['result']['0'])){
-    //                                 $data['user'][$key]->$mi[$timeth]['meet'] = '0';
-    //                             }else{
-    //                                 $data['user'][$key]->$mi[$timeth]['meet'] = '1';
-    //                             }
-                                
-    //                             // บันทึกการประชุม
-
-    //                             // รูปภาพ
-    //                             $data['user'][$key]->$mi[$timeth]['picture_meet'] = '0';
-
-    //                             foreach ($data['result'] as $key_result => $value_result) {
-
-    //                                 if($value_result->picture_meet != '' AND $value_result->picture_meet != 's:0:"";'){
-    //                                     $data['user'][$key]->$mi[$timeth]['picture_meet'] = '1';
-    //                                 }
-    //                             }
-    //                             // รูปภาพ
-    //                     }
-
-            
-    //                 if($i_m == '13'){
-    //                     $i_m = '01';
-    //                 }
-    //             }
-                
-    //         }
-    //         return view('admin::secure_scor.secure_scor_excell',$data);
-    // }
 
     /**
      * Store a newly created resource in storage.
